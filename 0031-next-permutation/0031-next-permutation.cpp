cfbdaf3b45561696b2bada1878a706f79bc50c5a
AnitBhattacharjee <anitbhattacharjee684@gmail.com>
2023-10-27 12:08:36 +0530

    Time: 0 ms (100.00%), Space: 12.7 MB (5.03%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)
-        {
-            cout<<nums[i];
-        }
+         next_permutation(nums.begin(),nums.end());
     }
 };
